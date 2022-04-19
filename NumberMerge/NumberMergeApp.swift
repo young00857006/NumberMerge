f5eb00dac379e03ccd753bfcba417596c69304a0
T.Y.Kuan <tingyoungkuan@gmail.com>
2022-04-19 14:46:50 +0800

    Initial Commit

---
+++ NumberMerge/NumberMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberMergeApp.swift
+//  NumberMerge
+//
+//  Created by tingyang on 2022/4/19.
+//
+
+import SwiftUI
+
+@main
+struct NumberMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
