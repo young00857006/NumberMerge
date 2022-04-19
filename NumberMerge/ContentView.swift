f2b4d2f1bd0270b12b361307b7bf1b067b1bfde7
T.Y.Kuan <tingyoungkuan@gmail.com>
2022-04-19 17:02:25 +0800

    微修

--- NumberMerge/ContentView.swift
+++ NumberMerge/ContentView.swift
@@ -16,7 +16,11 @@ struct ContentView: View {
     @State private var datas = [Data]()
     @State private var number = 1
     @State private var score = 0
-    func initialRec (){
+    @State private var isFull = false
+    func initial(){
+        number = 1
+        score = 0
+        isFull = false
         datas.removeAll()
         for i in 1...25{
             if(i==12){
@@ -41,12 +45,9 @@ struct ContentView: View {
         var down = index + 5
         var right = index + 1
         var left = index - 1
-        let target = datas[index].name
+        var target = datas[index].name
         var originScore = score
         
-//        if(up >= 0 && down <= 24 && index < right%5 && index > left%5){
-//            while
-//        }
         while true{
             
             if(up >= 0 && target == datas[up].name){
@@ -57,31 +58,41 @@ struct ContentView: View {
                 datas[down].name = ""
                 score += 1
             }
-            if(index%5 < right%5 && target == datas[right].name){
+            if(right<25 && index%5 < right%5 && target == datas[right].name){
                 datas[right].name = ""
                 score += 1
             }
-            if(index%5 > left%5 && target == datas[left].name){
+            if(left>=0 && index%5 > left%5 && target == datas[left].name){
                 datas[left].name = ""
                 score += 1
             }
             if(score>originScore){
                 datas[index].name = "\(Int(target)! + 1)"
                 originScore = score
+                target = datas[index].name
+                isFull = false
             }
             else{
                 break
             }
-            
         }
+    }
+    
+    func judgeFull()->Bool{
+        var isFull = true
+        for i in 0..<25{
+            if(datas[i].name == ""){
+                isFull = false
+                break
+            }
+        }
+        return isFull
     }
     
     var body: some View {
         VStack{ //Recatangle
             Button("start"){
-                initialRec()
-                number = 1
-                score = 0
+                initial()
             }
             Text("\(score)")
             let columns = Array(repeating: GridItem(), count: 5)
@@ -93,15 +104,26 @@ struct ContentView: View {
                         .padding(5)
                         .overlay(Text("\(data.name)"))
                         .onTapGesture {
-                            addNumber(index: index)
-                            generateNum()
-                            mergeNum(index: index)
+                            if(data.name == ""){
+                                addNumber(index: index)
+                                generateNum()
+                                mergeNum(index: index)
+                                isFull = judgeFull()
+                            }
                         }
                 }
             }
             
             Text("\(number)")
         }
+        .alert("Your Score : \(score) !!", isPresented: $isFull, actions: {
+            HStack{
+                Button("ok"){}
+                Button("restart"){
+                    initial()
+                }
+            }
+        })
     }
 }
 
